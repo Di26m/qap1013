83f723445d2338fd4e0c6796157e7b5bc39305a2
Di26m <express_musik@mail.ru>
2023-01-12 18:20:16 +1000

    16 модуль ООП

---
+++ test project/16 модуль/circle.py
@@ -0,0 +1,5 @@
+class Circle:
+    def __init__(self, a):
+        self.a = a
+    def get_area_circle(self):
+        return self.a ** 2 * 3.14
---
+++ test project/16 модуль/circle_2.py
@@ -0,0 +1,7 @@
+from circle import Circle
+
+circle_1 = Circle(3)
+circle_2 =Circle(4)
+
+print(circle_1.get_area_circle())
+print(circle_2.get_area_circle())
