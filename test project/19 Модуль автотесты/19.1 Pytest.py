55da955d5f67e1d86c747cffb585c98e448ef664
Di26m <express_musik@mail.ru>
2023-02-07 20:23:48 +1000

    19 requests

---
+++ test project/19 Модуль автотесты/19.1 Pytest.py
@@ -0,0 +1,18 @@
+import requests
+import  json
+status ='available'
+
+# res = requests.get(f"https://petstore.swagger.io/v2/pet/findByStatus?status={status}", headers = {'accept': 'application/json'})
+res = requests.get(f"https://petstore.swagger.io/v2/pet/findByStatus", params={'status': 'available'}, headers={'accept': 'application/json'})
+
+if 'application/json' in res.headers['Content-Type']:
+    res.json()
+else:
+    res.text
+print(res.status_code)
+print(res.text)
+print(res.json())
+print(type(res.json()))
+url =
+headers = {'accept': 'application/json'}
+res = requests.post(url, headers=headers, data=data)
