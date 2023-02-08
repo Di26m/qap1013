e550abafb917eb9716a1f3c839d269cf9553bd91
Di26m <express_musik@mail.ru>
2023-02-08 22:59:23 +1000

    19

---
+++ 19.7. Итоговое практическое задание/settings.py
@@ -0,0 +1,11 @@
+valid_email = 'zzzzz@mail.ru'
+valid_password = 'zzzzz'
+
+invalid_email = '444432ssd@yandex.ru'
+invalid_password = 'fdsdff'
+
+# valid_email = "vasya@mail.com"
+# valid_password = "12345"
+
+# valid_email = 'forteslesson@gmail.com'
+# valid_password = 'qwerty'
---
+++ test project/QAP_PetFriensTesting-master/settings.py
@@ -0,0 +1,7 @@
+
+
+valid_email = "vasya@mail.com"
+valid_password = "12345"
+
+# valid_email = "zzzzz@mail.com"
+# valid_password = "zzzzz"
